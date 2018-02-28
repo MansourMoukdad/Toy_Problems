222674533a0c0b98a4c6ef8af35b738d2da40a65
Nader Nasr <nadernasr7@gmail.com>
2018-02-28 10:02:45 +0200

    swap

---
+++ swap.js
@@ -0,0 +1,20 @@
+	
+	// Given a string, swap the case for each of the letters.
+	// IbrAHiM --> iBRahIm
+	// ToYPRoblEm --> tOyprOBLeM
+	function swap(input){
+		
+		var str="";
+		
+		var res = str.replace(str,function(string){
+		for (var i = 0; i < input.length; i++) {
+			if(input===input.toLowerCase(){
+				str= res;
+			}
+								
+			return string.toUpperCase();
+		});
+	
+	}
+
+ 
