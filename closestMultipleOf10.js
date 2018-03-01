9a3119f36445a32cd9eb387fa4fa938b57fce8a5
MansourMoukdad <m_sy@outlook.com>
2018-03-01 10:03:11 +0200

    last

---
+++ closestMultipleOf10.js
@@ -0,0 +1,17 @@
+ // Given a number return the closest number to it that is divisible by 10.
+ // Example :
+	// 22 ===> 20
+	// 25 ===> 30
+	// 37 ===> 40
+
+ function closestMultipleOf10(num) {
+ 	var closest = Math.round(num)
+ 	
+ 	if(num%2===0 && closest){
+ 	}
+
+return  Math.round(num);
+
+
+ }
+
